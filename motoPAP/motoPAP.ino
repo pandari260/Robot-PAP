e1d2e5f5d1b70e50b9d6114482be7c63aaeac185
Pandari <pandari260@gmail.com>
2018-12-17 18:34:47 -0300

    se modifico al app por completo

--- motoPAP/motoPAP.ino
+++ motoPAP/motoPAP.ino
@@ -1,42 +1,60 @@
+#include <Pushbutton.h>
+
+//-------- CONFIG.PINES -----------
 const int dirPin = 4;
 const int stepPin = 3;
- 
-const int steps = 200;
+const int finCarrera = 2;
+Pushbutton button(finCarrera);
+
+//-------- DATOS ------------------
+
+const int steps = 1200;
+const int microSeg = 1000;
 int stepDelay;
- 
+
+
 void setup() {
-   // Marcar los pines como salida
+ 
    pinMode(dirPin, OUTPUT);
    pinMode(stepPin, OUTPUT);
+
 }
 
-int converToDeg(int n){
-  
-  }
+void moverMotor(){
+ 
+   digitalWrite(stepPin,HIGH);
+   delayMicroseconds(microSeg);
+   digitalWrite(stepPin,LOW);
+   delayMicroseconds(microSeg);  
+}
+
 
 void loop() {
-   //Activar una direccion y fijar la velocidad con stepDelay
-   digitalWrite(dirPin, HIGH);
-   for(int i=0; i<=800; i++){
+  
+     digitalWrite(dirPin, LOW);  
+      
+     while(!button.isPressed()){
+
+        moverMotor();
+       
+       }
+
+     while(button.isPressed()){
+
+       delay(1000);
+       digitalWrite(dirPin, HIGH);
+       
+       for(int i=0; i<=steps; i++){
+         
+        moverMotor();
+        
+       }
+       
+     delay(2000);
+
+  }
+
+ 
    
-     digitalWrite(stepPin,HIGH);
-     delayMicroseconds(1000);
-     //delay(15);
-     digitalWrite(stepPin,LOW);
-     //delay(15);
-     delayMicroseconds(1000);
-   }
-   delay(2000);
-   digitalWrite(dirPin, LOW);
-   for(int i=0; i<=800; i++){
-     //3200 = 360 grados
-     
-     digitalWrite(stepPin,HIGH);
-     delayMicroseconds(1000);
-     //delay(15);
-     digitalWrite(stepPin,LOW);
-     //delay(15);
-     delayMicroseconds(1000);
-   }
-   delay(2000);
+  
 }
